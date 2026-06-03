3c60c32bf4257825086ef70be7d236d32255fa2b
Apishrana <apishrana682@gmail.com>
2026-06-03 13:56:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const { App } = require('@slack/bolt');
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
+});
+
+app.command('/dsb-calc', async ({ command, ack, respond }) => {
+    const comm = command;
+    await ack();
+    // const latency = Date.now() - start;
+    await respond({ text: `${eval(comm)}` });
+});
+
+(async () => {
+    await app.start();
+    console.log('bot is running!');
+})();
